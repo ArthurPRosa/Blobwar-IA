98bcc4852393bc34263fa7514bad6a332bf72752
Arthur Rosa <arthur.rosa@grenoble-inp.org>
2023-03-27 17:20:35 +0200

    fin minmax

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 //! rust alpha - beta implementation for the blobwar game.
 #![deny(missing_docs)]
 #![warn(clippy::all)]
-#![feature(test)]
 
 pub mod board;
 pub mod configuration;
@@ -9,4 +8,3 @@ pub(crate) mod positions;
 pub(crate) mod shmem;
 pub mod strategy;
 
-extern crate test;
