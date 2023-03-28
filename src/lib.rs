e2fb9f87b69c837e18168ebd219c02fe28783ddf
Hadrien Eyraud <hadrien.eyraud@grenoble-inp.org>
2023-03-28 12:50:20 +0200

    fix: feature-test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
 //! rust alpha - beta implementation for the blobwar game.
 #![deny(missing_docs)]
 #![warn(clippy::all)]
-#![feature(test)]
 
 pub mod board;
 pub mod configuration;
 pub(crate) mod positions;
 pub(crate) mod shmem;
 pub mod strategy;
-
-extern crate test;
