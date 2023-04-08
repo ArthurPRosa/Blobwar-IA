cff40b1b07bb92b24c54516370e011584b443391
Arthur Rosa <arthur.rosa@grenoble-inp.org>
2023-04-08 23:58:05 +0200

    Debut correction memoire meilleur mouvement

--- src/strategy/alphabeta.rs
+++ src/strategy/alphabeta.rs
@@ -1,20 +1,27 @@
 //! Alpha - Beta algorithm.
 use std::fmt;
-use std::u128::MAX;
-use std::u32::MIN;
 
 use super::Strategy;
 use crate::configuration::{Configuration, Movement};
 use crate::shmem::AtomicMove;
 
-fn alpha_beta_rec(player: bool, state: &Configuration, depth: u8, alpha: i8, beta: i8) -> i8 {
+fn alpha_beta_rec(
+    player: bool,
+    state: &Configuration,
+    depth: u8,
+    alpha: i8,
+    beta: i8,
+) -> Option<(i8, Option<Movement>)> {
     if depth == 0 {
-        return state.value()
-            * if state.current_player == player {
-                1
-            } else {
-                -1
-            };
+        return Some((
+            state.value()
+                * if state.current_player == player {
+                    1
+                } else {
+                    -1
+                },
+            None,
+        ));
     }
 
     let ok_moves = state.movements().filter(|mov| state.check_move(mov));
@@ -23,53 +30,63 @@ fn alpha_beta_rec(player: bool, state: &Configuration, depth: u8, alpha: i8, bet
 
     // If no move is doable, return the value
     if check_moves_size.peek() == None {
-        return state.value()
-            * if state.current_player == player {
-                1
-            } else {
-                -1
-            };
+        return Some((
+            state.value()
+                * if state.current_player == player {
+                    1
+                } else {
+                    -1
+                },
+            None,
+        ));
     }
-
-    if state.current_player != player {
+    let (_, _, val, mov) = if state.current_player != player {
         check_moves_size
-            .try_fold((alpha, beta, i8::MAX), |(alpha, beta, v), mov| {
-                let resval = alpha_beta_rec(player, &state.play(&mov), depth - 1, alpha, beta);
-                let mut new_v = v;
-                let mut new_beta = beta;
-                if resval < v {
-                    new_v = resval
-                };
-                if new_v <= alpha {
-                    return Err((alpha, beta, new_v));
-                };
-                if new_v < beta {
-                    new_beta = new_v
-                };
-                Ok((alpha, new_beta, new_v))
+            .try_fold((alpha, beta, i8::MAX, None), |(alpha, beta, v, old_mov), new_mov| {
+                //We use try_fold to be able to break from the fold, and we simply return the result from the last Ok or first Err with the identity in unwrap_or_else
+                if let Some((resval, _)) =
+                    alpha_beta_rec(player, &state.play(&new_mov), depth - 1, alpha, beta)
+                {
+                    let new_v = v.min(resval);
+                    if new_v < alpha {
+                        return Err((alpha, beta, new_v, Some(new_mov)));
+                    };
+                    if new_v < beta {
+                        (new_v, Some(new_mov))
+                    } else {
+                        (alpha, old_mov)
+                    };
+                    Ok((alpha, beta.min(new_v), new_v, Some(new_mov)))
+                } else {
+                    Ok((alpha, beta, v, None))
+                }
             })
-            .unwrap_or_else(|(alpha, beta, v)| (alpha, beta, v))
-            .2
+            .unwrap_or_else(|a| a)
     } else {
         check_moves_size
-            .try_fold((alpha, beta, i8::MAX), |(alpha, beta, v), mov| {
-                let resval = alpha_beta_rec(player, &state.play(&mov), depth - 1, alpha, beta);
-                let mut new_v = v;
-                let mut new_alpha = alpha;
-                if resval > v {
-                    new_v = resval
-                };
-                if new_v >= beta {
-                    return Err((alpha, beta, new_v));
-                };
-                if new_v > alpha {
-                    new_alpha = new_v
-                };
-                Ok((new_alpha, beta, new_v))
+            .try_fold((alpha, beta, i8::MIN, None), |(alpha, beta, v, old_mov), new_mov| {
+                //We use try_fold to be able to break from the fold, and we simply return the result from the last Ok or first Err with the identity in unwrap_or_else
+                if let Some((resval, _)) =
+                    alpha_beta_rec(player, &state.play(&new_mov), depth - 1, alpha, beta)
+                {
+                    let new_v = v.max(resval);
+                    if new_v > beta {
+                        return Err((alpha, beta, new_v, Some(new_mov)));
+                    }
+                    let (new_alpha, best_mov) =
+                    if new_v > alpha {
+                        (new_v, Some(new_mov))
+                    } else {
+                        (alpha, old_mov)
+                    };
+                    Ok((new_alpha, beta, new_v, best_mov))
+                } else {
+                    Ok((alpha, beta, v, None))
+                }
             })
-            .unwrap_or_else(|(alpha, beta, v)| (alpha, beta, v))
-            .2
-    }
+            .unwrap_or_else(|a| a)
+    };
+    Some((val, mov))
 }
 
 /// Anytime alpha beta algorithm.
@@ -95,23 +112,7 @@ impl fmt::Display for AlphaBeta {
 
 impl Strategy for AlphaBeta {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
-        state.movements().filter(|mov| state.check_move(mov))
-            .try_fold((i8::MIN, i8::MAX, i8::MAX, None), |(alpha, beta, v, _), mov| {
-                let resval = alpha_beta_rec(state.current_player, &state.play(&mov), self.0 - 1, alpha, beta);
-                let mut new_v = v;
-                let mut new_alpha = alpha;
-                if resval > v {
-                    new_v = resval
-                };
-                if new_v >= beta {
-                    return Err((alpha, beta, new_v, Some(mov)));
-                };
-                if new_v > alpha {
-                    new_alpha = new_v
-                };
-                Ok((new_alpha, beta, new_v, Some(mov)))
-            })
-            .unwrap_or_else(|(alpha, beta, v, mov)| (alpha, beta, v, mov))
-            .3
+        let (_, mov) = alpha_beta_rec(state.current_player, &state, self.0, i8::MIN, i8::MAX)?;
+        mov
     }
 }
